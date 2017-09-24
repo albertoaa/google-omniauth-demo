86b4ee189b145cbc02c4c46afa62139074bf9f84
Alberto Aragon <alberto@alturasoluciones.com>
2017-09-24 08:58:39 -0500

    Setting up and adding method to User model to store or update the user

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, "605500251554-0gq5ca7v2na2e15ubn2ueet01m77a0vd.apps.googleusercontent.com", "Azwqu-uWMp6nBL-q1JI2_Wvu"
+end
